ce795efebe26ee76367e21a735dffe37669ec174
Kyle Walker <kyle@walker-data.com>
2023-11-26 11:46:42 -0600

    commit saved raster script

--- scripts/day-21-raster.R
+++ scripts/day-21-raster.R
@@ -6,20 +6,22 @@ library(leaflet)
 isos <- mb_isochrone(
   location = "One Bowerman Dr, Beaverton, OR 97005",
   profile = "driving",
-  time = 1:60
+  time = 1:45
 )
 
-pal <- colorNumeric("rocket", isos$time, na.color = "transparent")
+pal <- colorNumeric("plasma", isos$time, na.color = "transparent")
 
-isos_proj <- st_transform(isos, 32615)
+isos_proj <- st_transform(isos, 32618)
 
 template <- raster(isos_proj, resolution = 100)
 
 iso_surface <- fasterize(isos_proj, template, field = "time", fun = "min")
 
 leaflet() %>%
-  addMapboxTiles(style_id = "streets-v11", username = "mapbox") %>%
+  addMapboxTiles(style_id = "light-v9",
+                 username = "mapbox",
+                 scaling_factor = "0.5x") %>%
   addRasterImage(iso_surface, colors = pal, opacity = 0.5) %>%
   addLegend(values = isos$time, pal = pal,
-            title = "Drive-time to NRG Arena")
+            title = "Drive-time to<br>Nike HQ")
 
